7be330ea9a0311eda40f1a9c4f11a6421c636d09
futamiyuuki <aa805766@gmail.com>
2017-12-23 12:47:34 -0800

    Update channel info

--- events.go
+++ events.go
@@ -17,13 +17,32 @@ func processEvents(msg *sqs.Message) {
 	}
 
 	if m.EventType == "video_click" {
-		fmt.Printf("message: %+v", m)
-		fmt.Println("video clicked")
+		fmt.Printf("video clicked, message: %+v\n", m)
 		var ch ChannelModel
 		fmt.Println("id:", m.ChannelID)
-		if err := dbc.Find(bson.M{"_id": m.ChannelID}).One(&ch); err != nil {
-			log.Fatal(err)
+		if err := dbc.FindId(m.ChannelID).One(&ch); err != nil {
+			fmt.Println("Channel not found, creating new channel")
+			cms := []CategoryModel{}
+			cms = append(cms, CategoryModel{
+				m.VideoCategory,
+				1,
+			})
+			ch.ID = m.ChannelID
+			ch.Categories = cms
+			if err := dbc.Insert(&ch); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Println("Channel found, updating channel", ch.ID)
+			for i, cat := range ch.Categories {
+				if cat.Category == m.VideoCategory {
+					fmt.Printf("Increment Category: %s %d\n", cat.Category, cat.Count)
+					ch.Categories[i].Count++
+				}
+			}
+			fmt.Printf("%+v\n", ch)
+			dbc.UpdateId(ch.ID, bson.M{"$set": ch})
 		}
-		fmt.Printf("channel: %+v", ch)
+		fmt.Printf("channel: %+v\n", ch)
 	}
 }
